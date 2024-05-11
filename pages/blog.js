fc5cfd945c6ad525d4bd3176a47458611b98ba1a
RoshanSawlani <roshansawlani@gmail.com>
2024-05-11 14:07:53 +0530

    Creating blog and blogpost page

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import  styles  from '@/styles/Blog.module.css';
+import Link from 'next/link';
 const Blog = () => {
   return (
-    <div>This is Blog</div>
+    <div className={styles.container}>
+      <main className={styles.main}>
+    <div className={styles.blogs}>
+          <div className={styles.blogItem}>
+      <Link href={'/blogpost/learn-JavaScript'}>
+            <h3 className={styles.blogItemh3}>How to learn JavaScript in 2024?</h3></Link>
+            <p>JavaScript is the language used to design logic for the web</p>
+          </div>
+          <div className={styles.blogItem}>
+            <h3>How to learn JavaScript in 2024?</h3>
+            <p>JavaScript is the language used to design logic for the web</p>
+          </div>
+          <div className={styles.blogItem}>
+            <h3>How to learn JavaScript in 2024?</h3>
+            <p>JavaScript is the language used to design logic for the web</p>
+          </div>
+          <div className={styles.blogItem}>
+            <h3>How to learn JavaScript in 2024?</h3>
+            <p>JavaScript is the language used to design logic for the web</p>
+          </div>
+        </div>
+        </main>
+        </div>
   )
 }
 
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import styles from '@/styles/BlogPost.module.css';
+
 const slug = () => {
     const router = useRouter()
     const { slug } = router.query
     return (
-        <div>{slug}</div>
+        <div className={styles.container}>
+            <main className={styles.main}>
+            <h1 className={styles.heading}>Title of the page {slug}</h1>
+            <hr />
+            <div>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid natus laudantium tempora dignissimos velit incidunt hic. Ullam in fugiat dolorum sunt, earum culpa.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nulla amet delectus, quia unde ad. Cumque praesentium beatae consequuntur vero quisquam, mollitia iusto debitis. Ullam placeat, incidunt molestiae repellat id commodi, minus perferendis tempore vitae praesentium non provident in a vero dolores sequi. Maiores, repellendus?
+                </div>
+                </main>
+        </div>
     )
 }
 
