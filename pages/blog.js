77a6f786c0def33dd53f72e523260e24379ba49c
RoshanSawlani <roshansawlani@gmail.com>
2024-05-11 14:50:51 +0530

    Adding blog jsons as data

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import  styles  from '@/styles/Blog.module.css';
 import Link from 'next/link';
+
+
+// Step 1 : Collect all the files from blog data directory
+// Step 2 : Iterate through them and display them
 const Blog = () => {
   return (
     <div className={styles.container}>
@@ -8,19 +12,19 @@ const Blog = () => {
     <div className={styles.blogs}>
           <div className={styles.blogItem}>
       <Link href={'/blogpost/learn-JavaScript'}>
-            <h3 className={styles.blogItemh3}>How to learn JavaScript in 2024?</h3></Link>
+            <h2 className={styles.blogItemh3}>How to learn JavaScript in 2024?</h2></Link>
             <p>JavaScript is the language used to design logic for the web</p>
           </div>
           <div className={styles.blogItem}>
-            <h3>How to learn JavaScript in 2024?</h3>
+            <h2>How to learn JavaScript in 2024?</h2>
             <p>JavaScript is the language used to design logic for the web</p>
           </div>
           <div className={styles.blogItem}>
-            <h3>How to learn JavaScript in 2024?</h3>
+            <h2>How to learn JavaScript in 2024?</h2>
             <p>JavaScript is the language used to design logic for the web</p>
           </div>
           <div className={styles.blogItem}>
-            <h3>How to learn JavaScript in 2024?</h3>
+            <h2>How to learn JavaScript in 2024?</h2>
             <p>JavaScript is the language used to design logic for the web</p>
           </div>
         </div>
