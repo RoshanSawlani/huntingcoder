19d15e72f2e485887dab4668a7d82fa3ee80f7bc
RoshanSawlani <roshansawlani@gmail.com>
2024-05-13 21:11:34 +0530

    using api endpoints of blog to fetch it on frontend

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,8 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
-export default function handler(req, res) {
-  fs.readdir('blogdata',(err,data)=>{
+export default async function handler(req, res) {
+  /* fs.promises.readdir('blogdata',(err,data)=>{
     console.log(data)
-    res.status(200).json( data);
-  })  
+    
+    data.forEach((item)=>{
+      console.log(item)
+      fs.readFile(('blogdata/' + item ),(d)=>{
+        allBlogs.push(d)
+      })
+    })
+    res.status(200).json(allBlogs);
+  })   */
+  let data = await fs.promises.readdir('blogdata')
+  let myFile
+  let allBlogs = []  
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+      console.log(item)
+      myFile = await fs.promises.readFile(('blogdata/' + item ),'utf-8')
+      allBlogs.push(JSON.parse(myFile))
+  } 
+    res.status(200).json(allBlogs);
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import  styles  from '@/styles/Blog.module.css';
 import Link from 'next/link';
 
@@ -6,28 +6,29 @@ import Link from 'next/link';
 // Step 1 : Collect all the files from blog data directory
 // Step 2 : Iterate through them and display them
 const Blog = () => {
+  const [blogs,setBlogs] = useState([])
+  useEffect(()=>{
+    console.log('useEffect is running')
+    fetch('http://localhost:3004/api/blogs').then((a)=>{
+      return a.json();})
+      .then((parsed)=>{
+        console.log(parsed)
+        setBlogs(parsed)
+    })
+  },[])
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-    <div className={styles.blogs}>
-          <div className={styles.blogItem}>
-      <Link href={'/blogpost/learn-JavaScript'}>
-            <h2 className={styles.blogItemh3}>How to learn JavaScript in 2024?</h2></Link>
-            <p>JavaScript is the language used to design logic for the web</p>
-          </div>
-          <div className={styles.blogItem}>
-            <h2>How to learn JavaScript in 2024?</h2>
-            <p>JavaScript is the language used to design logic for the web</p>
-          </div>
-          <div className={styles.blogItem}>
-            <h2>How to learn JavaScript in 2024?</h2>
-            <p>JavaScript is the language used to design logic for the web</p>
-          </div>
-          <div className={styles.blogItem}>
-            <h2>How to learn JavaScript in 2024?</h2>
-            <p>JavaScript is the language used to design logic for the web</p>
-          </div>
-        </div>
+        {blogs.map((blogItem)=>{
+          return <> <div className={styles.blogs}>
+            <div className={styles.blogItem} key={blogItem.slug}>
+          <Link href={`/blogpost/${blogItem.slug}`}>
+                <h2 className={styles.blogItemh3}>{blogItem.title}</h2></Link>
+                <p className={styles.blogItemp}>{blogItem.content.substr(0,140)}...</p>
+              </div>
+              </div>
+              </>
+        })}
         </main>
         </div>
   )
