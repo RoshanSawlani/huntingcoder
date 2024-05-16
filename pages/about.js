7c05a213ad8d75be3f8a82a7bfb4ee89701c6a40
RoshanSawlani <roshansawlani@gmail.com>
2024-05-16 20:38:36 +0530

    Designing the about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import styles from '../styles/About.module.css'
 
 const About = () => {
   return (
-    <div>
-        This is my about component
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Hunting Coder</h1>
+        <h2>Introduction</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum aspernatur eveniet expedita sed, magnam illum neque necessitatibus facilis recusandae inventore nihil architecto est velit consequuntur impedit incidunt optio voluptates consequatur magni? Cumque, ipsa at. Qui molestiae porro nihil neque praesentium id ab ipsam iure ullam explicabo sapiente odit autem architecto tempora accusantium labore blanditiis itaque, nisi tenetur, sed quam officiis fugiat numquam? Ex blanditiis aperiam harum sapiente maxime, vitae, quisquam corrupti eius soluta atque eos exercitationem. Inventore officiis, saepe consequatur doloremque ab ducimus rerum nostrum. Quibusdam error voluptates neque, atque, aspernatur, recusandae molestiae magnam placeat porro dignissimos est beatae hic officia explicabo nobis optio quasi nostrum assumenda consectetur! Consequuntur veniam eos ex earum dolore explicabo maiores possimus eaque maxime, saepe cumque, mollitia ut ab, assumenda magni odio! Enim facilis nisi sequi accusantium adipisci animi, quod fuga repellat nulla et officiis consectetur ipsam doloremque mollitia ab accusamus. Alias sit quia dignissimos explicabo laudantium, repudiandae illo, esse ipsum animi nesciunt reiciendis non asperiores nostrum, consequuntur exercitationem voluptatibus iure aperiam. Non consectetur sunt facilis cum dolorem illo suscipit harum ipsa nihil magni ratione quos itaque officia voluptates, aspernatur veniam libero corruptive</p>
+        <h2>Services Offered</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum iste, dicta expedita ad dolorum nihil recusandae. Cum delectus, corrupti nisi repellat, praesentium maxime quisquam quod ab facilis, libero itaque totam quasi expedita illo!</p>
+        <p>We offer the following services:</p>
+        <ul>
+          <li>Service 1</li>
+          <li>Service 1</li>
+          <li>Service 1</li>
+          <li>Service 1</li>
+          <li>Service 1</li>
+          <li>Service 1</li>
+          <li>Service 1</li>
+          <li>Service 1</li>
+          <li>Service 1</li>
+        </ul>
+        <h2>Contact Us</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores officia doloribus quod. Ad iure ipsa at dignissimos ipsam aliquam id vitae rerum voluptas, excepturi perferendis expedita ab sunt consectetur dolore nostrum officia voluptatibus obcaecati placeat saepe iusto beatae. Alias, a officiis quaerat corrupti itaque suscipit at mollitia, fugiat minima nihil iusto inventore ex incidunt deleniti dolorum?</p>
     </div>
   )
 }
